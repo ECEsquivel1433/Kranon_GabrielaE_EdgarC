b053d3d499615f136aa4fb0c4bd6f83acc16c0ca
ECEsquivel1433 <edgarcoronaesq@gmail.com>
2023-05-23 16:02:28 -0600

    DLConection

---
+++ BL/Libro.cs
@@ -0,0 +1,29 @@
+﻿namespace BL
+{
+    public class Libro
+    {
+        //public static ML.Result AddEF(ML.Usuario usuario)
+        //{
+        //    ML.Result result = new ML.Result();
+
+        //    try
+        //    {
+        //        using (DL.EcoronaProgramacionNcapasContext context = new DL.EcoronaProgramacionNcapasContext())
+        //        {
+        //            int queryEF = context.Database.ExecuteSqlRaw($"UsuarioAdd '{usuario.UserName}','{usuario.Nombre}','{usuario.ApellidoPaterno}','{usuario.ApellidoMaterno}','{usuario.Email}','{usuario.Password}','{usuario.FechaNacimiento}','{usuario.Sexo}','{usuario.Telefono}','{usuario.Celular}','{usuario.CURP}','{usuario.Imagen}', {usuario.Rol.IdRol},'{usuario.Direccion.Calle}','{usuario.Direccion.NumeroInterior}','{usuario.Direccion.NumeroExterior}',{usuario.Direccion.Colonia.IdColonia},'{usuario.Estatus}'");
+        //            if (queryEF > 0)
+        //            {
+        //                result.Correct = true;
+        //            }
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        result.Correct = false;
+        //        result.ErrorMessage = "Ocurrio un error al insertar el usuario" + ex;
+        //    }
+        //    return result;
+        //}
+
+    }
+}
---
+++ ML/Libro.cs
@@ -0,0 +1,13 @@
+﻿namespace ML
+{
+    public class Libro
+    {
+
+        //public int IdLibro { get; set; } = default!;
+        //public string Imagen { get; set; } = default!;
+        //public List<object> Usuarios { get; set; } = default!;
+        //public ML.Autor Autor { get; set; } = default!;
+        //public ML.Editorial Editorial { get; set; } = default!;
+        //public bool Estatus { get; set; } = default!;
+    }
+}
