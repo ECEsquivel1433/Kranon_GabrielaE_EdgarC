b0dc8131809a60bc61a329225580118c077b0ba7
Cecilia Gabriela Escalona Chavarria <cescalonacha@gmail.com>
2023-05-24 10:57:52 -0600

    Vitas

--- ML/Libro.cs
+++ ML/Libro.cs
@@ -3,11 +3,11 @@
     public class Libro
     {
 
-        public int IdLibro { get; set; } = default!;
-        //public string Imagen { get; set; } = default!;
-        public List<object> Libros { get; set; } = default!;
-        //public ML.Autor Autor { get; set; } = default!;
-        //public ML.Editorial Editorial { get; set; } = default!;
-        //public bool Estatus { get; set; } = default!;
+        //public int IdLibro { get; set; } = default!;
+        ////public string Imagen { get; set; } = default!;
+        //public List<object> Libros { get; set; } = default!;
+        ////public ML.Autor Autor { get; set; } = default!;
+        ////public ML.Editorial Editorial { get; set; } = default!;
+        ////public bool Estatus { get; set; } = default!;
     }
 }
